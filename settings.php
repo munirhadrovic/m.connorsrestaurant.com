aa106269b1234706561f2ca4331ee475e032bb7e
unknown <nenadkonatar@live.ac.me>
2015-05-29 13:22:50 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,29 @@
+<?php
+    $adresa[0]="Connor Concepts, Inc.10911 Turkey Drive Knoxville, TN 37934 "; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+    $grad[0]="Knoxville";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+    $skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+    $nazivlokacije[0]="CORPORATE HEADQUARTERS";
+    $telephone[0]=8657772677;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="345 The Bridge Street Huntsville, AL 35806 "; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="Alabama";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="Alabama";
+	$telephone[1]=2563278425;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="10076 Gulf Center Drive Fort Myers, FL 33913"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="Florida";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="Florida";
+	$telephone[2]=2392672677;// Broj koji se poziva na dugme CALL US
+
+	$adresa[3]="10915 Turkey Drive Knoxville, TN 37934"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[3]="Tennessee";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[3]="Tennessee";
+	$telephone[3]=8659660933;// Broj koji se poziva na dugme CALL US
+
+	$companynumber=4;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
